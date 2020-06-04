3937af10a7c7afd1e66f7583049a7ec7b0b1abfa
Phil de Joux <philderbeast@gmail.com>
2020-06-04 15:52:04 -0400

    Create a new package, ghc-tcplugins-api.

---
+++ ghc-tcplugins-api/src-ghc-8.10/Internal/Evidence.hs
@@ -0,0 +1,14 @@
+module Internal.Evidence (evDFunApp', evCast', terms) where
+
+import Internal
+
+evDFunApp' :: DFunId -> [Type] -> [EvExpr] -> EvTerm
+evDFunApp' = evDFunApp
+
+evCast' :: EvTerm -> TcCoercion -> EvTerm
+evCast' (EvExpr e)  = evCast e
+evCast' EvTypeable{} = error "Can't evCast (EvTypeable _ _)"
+evCast' EvFun{} = error "Can't evCast (EvFun _ _ _ _)"
+
+terms :: Type -> Type -> [EvExpr]
+terms t1 t2 = [let (EvExpr e) = evByFiat "units" t1 t2 in e]
